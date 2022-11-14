47931783d47a24858706f367ba8a859cf0d62e5e
Heeseung <gmltmd0816@naver.com>
2022-11-14 16:55:23 +0900

    feat: 발행한 로또 번호 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+const MissionUtils = require("@woowacourse/mission-utils");
+const Lotto = require("./Lotto");
+
 class App {
-  play() {}
+  play() {
+    this.createLotto();
+  }
+
+  createLotto() {
+    const randomNumbers = MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6);
+    return this.printLotto(randomNumbers);
+  }
+
+  printLotto(numbers) {
+    numbers.sort((a, b) => { return a - b });
+    MissionUtils.Console.print(`[${numbers.join(", ")}]`);
+    console.log(`[${numbers.join(", ")}]`);
+  }
 }
+
+const app = new App();
+app.play();
 
 module.exports = App;
